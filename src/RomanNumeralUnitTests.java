10084084b3a510fd2b4e160b4debed3f00fb6299
yogevge <yogev14@hotmail.com>
2012-12-16 08:38:56 +0200

    now i did 8

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -3,16 +3,27 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 
-public class RomanNumeralUnitTests {
+public class RomanNumeralUnitTests 
+{
 
 	private RomanNumerals number = new RomanNumerals();
 
 	@Test
-	public void testThat1ReturnsI() {
+	public void return1to3Itests() 
+	{
 		assertEquals("I", number.toRoman(1));
 		assertEquals("II", number.toRoman(2));
 		assertEquals("III", number.toRoman(3));
+
 	}
-	
+	@Test
+	public void return4to8Vtests() 
+	{
+		assertEquals("IV", number.toRoman(4));
+		assertEquals("V", number.toRoman(5));
+		assertEquals("VI", number.toRoman(6));
+		assertEquals("VII", number.toRoman(7));
+		assertEquals("VIII", number.toRoman(8));
+	}
 
 }
