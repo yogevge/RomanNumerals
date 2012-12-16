ea5ddbdde0de2960947a7d41bf0a281c5104878b
yogevge <yogev14@hotmail.com>
2012-12-16 08:38:19 +0200

    now i did X

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,7 +1,47 @@
 
 public class RomanNumerals {
 
-	public String toRoman(int nmber) {
+	public String toRoman(int number)
+	
+	
+	{
+		String toRoman="";
+		
+		if(number>8)
+		{
+			while(number>=10)
+			{
+				toRoman+="X";
+				number-=10;
+			}
+			if(number==9)
+			{
+				toRoman+="IX";
+				number-=9;
+			}
+		}
+		if(number>3)
+		{
+			while(number>=5)
+			{
+				toRoman+="V";
+				number-=5;
+			}
+			if(number== 4)
+			{
+				toRoman+= "IV";
+				number-=4;
+			}
+
+		}
+		
+		if (number <= 3)
+		{
+			for(int i=0;i<number;i++)
+				toRoman+= "I";
+		}
+		return toRoman;
+	}
 		if (nmber == 1)
 			return "I";
 		return "II";
